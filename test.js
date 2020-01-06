2da8c67283afce4482b5042077970f568bebca79
unknown <hippolytejacque@hotmail.fr>
2020-01-06 11:42:41 +0100

    nouveaux tests pour la méthode hashIt avec les trois algorithmes sha1, sha256 et sha512

--- test.js
+++ test.js
@@ -7,14 +7,21 @@ test('BloomFilter est une classe', () => {
 test('on peut ajouter des éléments au storage', () => {
   const filter = new BloomFilter();
   expect(() => {
-    filter.add("test1");
+    filter.add("word1");
   }).not.toThrow();
+});
+
+test('hashIt retourne bel et bien un nombre entre 0 et 217 depuis une chaine hex crée par sha1, sha256 et sha512', () => {
+  const filter = new BloomFilter();
+  expect(filter.hashIt("word1", "sha1")).toEqual(5);
+  expect(filter.hashIt("word1", "sha256")).toEqual(102);
+  expect(filter.hashIt("word1", "sha512")).toEqual(24);
 });
 
 test('on peut vérifier si l\'élément est présent dans le storage', () => {
   const filter = new BloomFilter();
-  expect(filter.test("test1")).toEqual(false);
-  filter.add("test1");
-  expect(filter.test("test1")).toEqual(true);
-  expect(filter.test("test2")).toEqual(false);
+  expect(filter.test("word1")).toEqual(false);
+  filter.add("word1");
+  expect(filter.test("word1")).toEqual(true);
+  expect(filter.test("word2")).toEqual(false);
 });
